1f9598a980002e3a2235aacede9e6a3dab8c6311
apolloclark <apolloclark@gmail.com>
2019-06-10 10:13:47 -0400

    refactored Ansible playbook

--- build_packer_docker_ubuntu16.04.sh
+++ build_packer_docker_ubuntu16.04.sh
@@ -4,7 +4,7 @@
 export DOCKER_USERNAME=${DOCKER_USERNAME:=$(whoami)}
 export PACKAGE=${PACKAGE:=tomcat}
 export PACKAGE_VERSION=${PACKAGE_VERSION:="9.0.16"}
-export BASE_IMAGE=${BASE_IMAGE:="apolloclark/openjdk:11.0.1-ubuntu16.04"}
+export BASE_IMAGE=${BASE_IMAGE:="apolloclark/openjdk:11.0.2-ubuntu16.04"}
 export IMAGE_NAME=${IMAGE_NAME:="ubuntu16.04"}
 
 ./build_packer_docker.sh
